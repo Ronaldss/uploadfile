190e25f1bf8676ea0c260f6f3b4aaa1e3965c21d
ronald <ronaldcontact2019@gmail.com>
2021-06-11 21:59:11 -0300

    begin adding sound buttons

---
+++ js/sound.js
@@ -0,0 +1,8 @@
+//<img src="" onclick="TocarMusica()" />
+
+
+function TocarMusica(){
+    var audio1 = new Audio();
+    audio1.src = "../assets/music/Spring_In_My_Step.mp3";
+    audio1.play();
+}
