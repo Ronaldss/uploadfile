aaba3530750a9c9b2ae074e5f7f26e9a5cd3790a
unknown <ronaldcontact2019@gmail.com>
2021-07-02 13:39:12 -0300

    ajuste no volume do audio da musica

--- js/sound.js
+++ js/sound.js
@@ -10,7 +10,7 @@ function TocarMusicaTest(){
 function TocarMusica(){
   var audio1 = new Audio();
   audio1.src = "./assets/music/Spring_In_My_Step.mp3";
-  audio1.volume = 0.02;
+  audio1.volume = 0.4;
   audio1.play();
   console.log('Music...')
 };
